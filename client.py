8c6aae8b56a5822250f32c66a2c37b8d956c1a4e
Meghna <meghnaravikumar06@gmail.com>
2025-11-13 22:58:57 +0530

    fixed mini bug

--- client.py
+++ client.py
@@ -1,24 +1,16 @@
 import socket
 import time
-
 server_ip = "10.238.46.148"
-
 port = 9999
-
 client_name = input("Enter client name: ")
-
-# DISCOVER
 s = socket.socket()
 s.connect((server_ip, port))
 s.send("DISCOVER".encode())
 msg = s.recv(1024).decode()
 s.close()
-
 if msg.startswith("OFFER"):
     offered_ip = msg.split()[1]
     print(f"[DISCOVER] Server offered: {offered_ip}")
-
-    # REQUEST
     time.sleep(1)
     s = socket.socket()
     s.connect((server_ip, port))
@@ -26,16 +18,11 @@ if msg.startswith("OFFER"):
     s.send(req.encode())
     msg2 = s.recv(1024).decode()
     s.close()
-
     if msg2.startswith("ACK"):
         print(f"[REQUEST] ACK received. IP assigned: {offered_ip}")
         print("Client now using this IP...\n")
-
-        # simulate using IP (lease can expire automatically)
         print(f"Using IP for 15 seconds...")
         time.sleep(15)
-
-        # RELEASE manually (optional)
         s = socket.socket()
         s.connect((server_ip, port))
         rel = f"RELEASE {client_name}"
@@ -45,6 +32,5 @@ if msg.startswith("OFFER"):
         print(f"[RELEASE] {msg3}")
     else:
         print("Request denied or failed.")
-
 else:
     print("No IP available from server.")
